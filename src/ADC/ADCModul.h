3b1966769c08359264d14bf6eb92562c06d5cb09
Fagermaennchen <simonfagermann@yahoo.de>
2023-12-06 10:21:09 +0100

    Added todo

--- src/ADC/ADCModul.h
+++ src/ADC/ADCModul.h
@@ -10,6 +10,6 @@ void readADCvalue_routine(void); //Routine to read adc value from FIFO
 void setupADC(void);    // Setup the timer triggered ADC
 void changeTimeBase(int newTimeBase);   // Chandes the Timebase (input: newTimeBase [us])
 void startADC();    // Starts the timer triggered ADC
-int convertADCtoVolt(int adcVal); // Converts ADC Value into Voltage [mV]
+int convertADCtoVolt(int adcVal); // Converts ADC Value into Voltage [mV]   #TODO: In Cursormodul einbauen
 
 
